568b7e71bedf3c209c96333926accf87c2b0ea5b
Federico Vichachi <vichachifederico@gmail.com>
2025-06-30 16:06:25 -0300

    Añade icono y logo contextual al modo seleccionado

--- js/funcionalidades.js
+++ js/funcionalidades.js
@@ -3,7 +3,10 @@ const cuerpo = document.getElementById("cuerpo");
 const encabezadoPrincipal = document.getElementById("encabezado-principal");
 const navegacionCategoria = document.getElementById("navegacion-categoria");
 const seccionCategoria = document.getElementById("seccion-categoria");
-const footerPrincipal = document.getElementById("footer-principal")
+const footerPrincipal = document.getElementById("footer-principal");
+const logo = document.getElementById("logo");
+const iconoLunaSol = document.getElementById("luna-sol")
+const textoModo = document.getElementById("texto-modo")
 
 cambiarModo.addEventListener("click", () => {
     encabezadoPrincipal.classList.toggle("dark-theme");
@@ -11,4 +14,14 @@ cambiarModo.addEventListener("click", () => {
     navegacionCategoria.classList.toggle("dark-theme");
     seccionCategoria.classList.toggle("dark-theme");
     footerPrincipal.classList.toggle("dark-theme");
+
+    if (cuerpo.classList.contains("dark-theme")) {
+    logo.src = "assets/img/logo-dark.png";
+    iconoLunaSol.className = "bx bxs-sun"
+    textoModo.textContent = "Modo Claro";
+  } else {
+    logo.src = "assets/img/logo-light.png"; 
+    iconoLunaSol.className = "bx bxs-moon"
+    textoModo.textContent = "Modo Oscuro";
+  }
 });
